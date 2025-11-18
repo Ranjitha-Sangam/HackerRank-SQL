b9f1e5e0491cb2208d1c9860aa68c0d1682b057b
Ranjitha2710 <ranjithasangamss@gmail.com>
2025-11-18 15:21:05 +0530

    nov 18

---
+++ sql18.sql
@@ -0,0 +1,3 @@
+https://www.hackerrank.com/challenges/name-of-employees/
+
+SELECT name from Employee order by name asc;
---
+++ sql19.sql
@@ -0,0 +1,5 @@
+
+https://www.hackerrank.com/challenges/salary-of-employees/
+
+SELECT name from Employee where salary >2000 AND 
+months<10 order by employee_id;
