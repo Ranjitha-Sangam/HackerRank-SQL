5ed1d49898c341a7f3f6a6abae8e136ac0151ba8
Ranjitha2710 <ranjithasangamss@gmail.com>
2025-12-01 14:37:27 +0530

    dec 1

---
+++ sql36.sql
@@ -0,0 +1,7 @@
+https://www.hackerrank.com/challenges/binary-search-tree-1/
+SELECT N,
+    CASE WHEN P IS NULL THEN 'Root'
+    WHEN N NOT IN (SELECT P FROM BST WHERE P IS NOT NULL) THEN 'Leaf'
+    ELSE 'Inner'
+    END AS NodeType
+FROM BST ORDER BY N;
